eb0fad8d8734aa121e63b33b9714ced5aa7ea48c
jasonshiffler <jasonshiffler@yahoo.com>
2014-12-03 17:59:09 -0600

    moved some things around

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+/*********************
+ * index.php
+ *
+ *
+ * Dispatcher for MVC
+ *********************/
+
+session_start();
+
+if (isset($_GET["page"]))
+	$page = $_GET["page"];
+else
+	$page = "home";
+
+$path = __DIR__ . '/../privatecode/controller/' . $page . '.php';
+if (file_exists($path))
+	require($path);
+
+?>
